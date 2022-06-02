98ac6c963b05a1c6f962b4107fb90595b26728bd
Javier <jheisecke@bancognb.com.py>
2022-06-02 08:48:42 -0400

    adds project

---
+++ WhatToWatch/WhatToWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatToWatchApp.swift
+//  WhatToWatch
+//
+//  Created by Javier Heisecke on 2022-06-02.
+//
+
+import SwiftUI
+
+@main
+struct WhatToWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
